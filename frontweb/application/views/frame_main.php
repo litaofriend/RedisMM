5cf97ab6c30c4f72a62f3ce9228f5bb2e15e12f6
litaofriend <litaofriend@163.com>
2015-07-15 19:23:43 +0800

    commit infomation

--- frontweb/application/views/frame_main.php
+++ frontweb/application/views/frame_main.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta charset="utf-8">
-<title>社区搜索部Redis监控系统</title>
+<title>Redis监控管理系统</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="">
 <meta name="author" content="">
