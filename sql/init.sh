d611c6cdaa1edc5f7b65ff1bb424a12ba90f9fe0
litaofriend <litaofriend@163.com>
2015-07-15 19:10:26 +0800

    commit infomation

--- sql/init.sh
+++
@@ -1,3 +0,0 @@
-#cat create_redis_table.sql |mysql -uroot -proot95
-#echo "grant all on redis_monitor.* to redis_monitor@'%' identified by 'redis_monitor_pass';flush privileges;"|mysql -uroot -proot95
-
